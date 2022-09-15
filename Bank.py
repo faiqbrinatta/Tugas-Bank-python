6d380028a857de8f1e959c4bb8aa65a478d7d92b
faiqbrinatta <113093804+faiqbrinatta@users.noreply.github.com>
2022-09-15 21:34:33 +0700

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,17 @@
+# Fa'iq Zhafran Naufal Brinatta / 220535608468 / Teknik Informatika B
+
+greet = input("What's your greeting : ").lower().strip()
+x = greet.split(',')
+
+if (greet == ("hello")):
+    money = 0
+if (x[0]== "hello") :
+    money = 0
+elif (greet[0]==("h")):
+    money = 20
+else :
+    money = 100
+
+print("$", money)
+    
+    
