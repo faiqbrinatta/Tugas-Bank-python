0d6b0f375c144a55462c1dc75f2162d30669cbde
faiqbrinatta <113093804+faiqbrinatta@users.noreply.github.com>
2022-09-23 14:26:41 +0700

    Add files via upload

---
+++ Vowel.py
@@ -0,0 +1,13 @@
+#Fa'iq Zhafran Naufal Brinatta / 220535608468
+
+
+vokal = ("a", "i", "u", "e", "o", "A", "I","U","E","O")
+
+pesan = input("Apa pesan mu ? ")
+baru = ("")
+
+for letter in pesan :
+    if letter not in vokal:
+        baru += letter
+    
+print(baru)
