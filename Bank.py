e837fd3b510ca5e1d1882a94e26305573ee76e4c
faiqbrinattam <zhafranbrinatta@gmail.com>
2024-10-01 22:47:52 +0700

    Perubahan 1

--- Bank.py
+++ Bank.py
@@ -14,3 +14,20 @@ else :
 
 print("$", money)
 print("Tegar Ganteng")
+
+# # Fa'iq Zhafran Naufal Brinatta / 220535608468 / Teknik Informatika B
+
+# greet = input("What's your greeting : ").lower().strip()
+# x = greet.split(',')
+
+# if (greet == ("hello")):
+#     money = 0
+# if (x[0]== "hello") :
+#     money = 0
+# elif (greet[0]==("h")):
+#     money = 20
+# else :
+#     money = 100
+
+# print("$", money)
+# print("Tegar Ganteng")
