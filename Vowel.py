fec15c64d1f71f662d8594126d27cdb3ef604b8e
faiqbrinattam <zhafranbrinatta@gmail.com>
2024-10-01 22:52:23 +0700

    lesson

--- Vowel.py
+++ Vowel.py
@@ -11,3 +11,18 @@ for letter in pesan :
         baru += letter
     
 print(baru)
+
+
+# #Fa'iq Zhafran Naufal Brinatta / 220535608468
+
+
+# vokal = ("a", "i", "u", "e", "o", "A", "I","U","E","O")
+
+# pesan = input("Apa pesan mu ? ")
+# baru = ("")
+
+# for letter in pesan :
+#     if letter not in vokal:
+#         baru += letter
+    
+# print(baru)
